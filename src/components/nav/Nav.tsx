762b4d45d60e46e7cb5c7e57d4be36143b732de9
Adriel Simo <adrielsimo500@gmail.com>
2026-02-12 19:07:52 +0100

    Design du menu

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router"
+import { NAV_LINKS } from "../../utils/data"
+
+function Nav() {
+    return (
+        <nav className="z-10 bg-white shadow-md w-56 fixed flex flex-col justify-between top-0 left-0 bottom-0">
+            <Link to={'/private/declarations'}
+                className="bg-blue-600 text-center text-white uppercase rounded-sm py-5 font-extrabold ">
+                HEALTHcare </Link>
+            <ul>
+                {NAV_LINKS.map(({ to, label }, index) => (
+                    <li key={`navlink-${index}`} className={`border-b border-gray-300 ${index === 0 ? "border-t" : null}`}>
+                        < Link to={to} className="py-1 pl-2 hover:bg-gray-100 block" >
+                            {label}
+                        </Link>
+                    </li>
+                ))
+                }
+
+            </ul >
+            <button type="button" onClick={() => null} className="bg-red-700 rounded-sm text-white hover:text-red-700 py-2 font-normal hover:border hover:border-red-700 hover:bg-white">
+                Déconnexion
+            </button>
+        </nav >
+    )
+}
+
+export default Nav
--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
+import Nav from "../components/nav/Nav";
 
 function PrivateLayout() {
     return (
-        <section className="border border-red-300">
-            PrivateLayout
-            <Outlet />
+        <section className="border-4 border-red-700 min-h-screen">
+            <Nav />
+            <main className="wrapper pl-64 pr-10">
+                <Outlet />
+            </main>
         </section>
     )
 }
