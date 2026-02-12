ab105fe0d8fbc460636e80bf43994558d267e7cd
Adriel Simo <adrielsimo500@gmail.com>
2026-02-12 20:36:33 +0100

    Configuraion du layout

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -5,12 +5,12 @@ function Nav() {
     return (
         <nav className="z-10 bg-white shadow-md w-56 fixed flex flex-col justify-between top-0 left-0 bottom-0">
             <Link to={'/private/declarations'}
-                className="bg-blue-600 text-center text-white uppercase rounded-sm py-5 font-extrabold ">
+                className="bg-blue-600 text-center italic text-white uppercase rounded-sm py-5 font-extrabold ">
                 HEALTHcare </Link>
             <ul>
                 {NAV_LINKS.map(({ to, label }, index) => (
                     <li key={`navlink-${index}`} className={`border-b border-gray-300 ${index === 0 ? "border-t" : null}`}>
-                        < Link to={to} className="py-1 pl-2 hover:bg-gray-100 block" >
+                        < Link to={to} className="py-1 pl-2 italic font-bold hover:bg-gray-200 block" >
                             {label}
                         </Link>
                     </li>
@@ -18,7 +18,7 @@ function Nav() {
                 }
 
             </ul >
-            <button type="button" onClick={() => null} className="bg-red-700 rounded-sm text-white hover:text-red-700 py-2 font-normal hover:border hover:border-red-700 hover:bg-white">
+            <button type="button" onClick={() => null} className="bg-red-700  rounded-sm text-white hover:text-red-700 py-2 font-bold hover:border hover:border-red-700 hover:bg-white">
                 Déconnexion
             </button>
         </nav >
--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -6,6 +6,14 @@ function PrivateLayout() {
         <section className="border-4 border-red-700 min-h-screen">
             <Nav />
             <main className="wrapper pl-64 pr-10">
+                <header className="flex justify-between my-3 italic font-bold text-blue-500 text-2xl">
+                    <h3>
+                        Déclarations
+                    </h3>
+                    <h3>
+                        Adriel Simo
+                    </h3>
+                </header>
                 <Outlet />
             </main>
         </section>
