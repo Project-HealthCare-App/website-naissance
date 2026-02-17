da13567e31379e8b25c9cff33bd4d788f6106572
Adriel Simo <adrielsimo500@gmail.com>
2026-02-17 23:51:02 +0100

    filtrer les déclarations

--- src/hooks/use-declarations/useDeclarations.ts
+++ src/hooks/use-declarations/useDeclarations.ts
@@ -1,11 +1,14 @@
 import { search } from "@/services";
 import type { Declaration } from "@/types/Declaration";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function useDeclarations () {
+    const filterRef = useRef<any>(null);
     const [statusOrder, setStatusOrder] = useState(1);
     const [dateOrder, setDateOrder] = useState(1);
     const [declarations, setDeclarations] = useState<Declaration[]>([]);
+    const [filteredDeclarations, setFilteredDeclarations] = useState<Declaration[]>([]);
+
     const sortByStatus = () => {
      const sortedDeclarations = declarations.sort((itemOne: Declaration, itemTwo: Declaration) => {
             const {status: itemOneStatus} = itemOne;
@@ -33,6 +36,23 @@ function useDeclarations () {
         setDeclarations([...sortedDeclarations]);
     };
 
+
+
+    const filterDeclarations = () => {
+        const filter = filterRef.current.value.toLowerCase() || "";
+        if (filter.length >= 2) {
+           const filteredDeclarations = declarations.filter(item =>{
+                const {child: {firstName, lastName}} = item;
+                return ( 
+                    firstName.toLowerCase().indexOf(filter.toLowerCase()) > -1 
+                || lastName.toLowerCase().includes(filter.toLowerCase()))
+            });
+            setFilteredDeclarations([...filteredDeclarations]);
+        } else {
+            setFilteredDeclarations([...declarations]);
+        }
+    };
+
     const getDeclaration = async () => {
         const data = await search("declarations");
         setDeclarations(data);
@@ -41,7 +61,7 @@ function useDeclarations () {
         getDeclaration();
     }, []);
 
-    return { declarations, sortByStatus, sortByDate };
+    return { declarations, filteredDeclarations, filterRef, sortByStatus, sortByDate, filterDeclarations };
 }
 
 export { useDeclarations }
--- src/pages/Declarations.tsx
+++ src/pages/Declarations.tsx
@@ -1,16 +1,31 @@
 import { useDeclarations } from "@/hooks";
 import DeclarationsItems from "@/components/declarations/DeclarationsItems";
+import { Link } from "react-router-dom";
 
 function Declarations() {
-    const { declarations, sortByStatus, sortByDate } = useDeclarations();
+    const { filterRef, declarations, filteredDeclarations, sortByStatus, sortByDate, filterDeclarations } = useDeclarations();
     return (
-        <div className="border border-gray-200 bg-white shadow-md rounded-md">
-            <DeclarationsItems
-                declarations={declarations}
-                sortByStatus={sortByStatus}
-                sortByDate={sortByDate}
-            />
-        </div >
+        <>
+            <div className=" bg-white shadow-md rounded-md mb-3 flex items-center justify-between py-3 px-3">
+                <input type="text" name="rechercher" id="rechercher" placeholder="Rechercher par Nom ou Prénom de L'enfant"
+                    className="bg-gray-200 px-3 py-2 rounded-md w-96 outline-blue-600 "
+                    ref={filterRef}
+                    onKeyUp={filterDeclarations}
+                />
+                <Link to={"/private/declarations/new"}
+                    className=" bg-green-600 rounded-md block px-3 py-2 text-white">
+                    Nouvelle déclaration
+                </Link>
+            </div >
+            <div className="border border-gray-200 bg-white shadow-md rounded-md">
+                <DeclarationsItems
+                    declarations={
+                        filteredDeclarations.length ? filteredDeclarations : declarations}
+                    sortByStatus={sortByStatus}
+                    sortByDate={sortByDate}
+                />
+            </div >
+        </>
     );
 }
 
